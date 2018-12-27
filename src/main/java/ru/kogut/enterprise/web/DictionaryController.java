4c8fcb5fb5bb1be7fca311a0c8a836cf836c0bcc
Kogut Sergey <koguts@yandex.ru>
2018-12-27 16:37:37 +0300

    init 3

---
+++ src/main/java/ru/kogut/enterprise/web/DictionaryController.java
@@ -0,0 +1,10 @@
+package ru.kogut.enterprise.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dictionary")
+public class DictionaryController {
+
+}
